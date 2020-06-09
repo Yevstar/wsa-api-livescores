2ff929632a5b8ea44e5dbd98925e7266135a410d
Yevstar <fedchyshynyevhen@gmail.com>
2020-06-10 00:02:50 +1000

    typo

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -131,7 +131,7 @@ export default class UserService extends BaseService<User> {
         }
 
         let orgEntityId = EntityType.ORGANISATION;
-        query.andWhere('ure.entityTypeId = :entityId', {orgEntityId})
+        query.andWhere('ure.entityTypeId = :orgEntityId', {orgEntityId})
         .andWhere('co.organisationId = :organisationId', {organisationId});
 
         if (search) {
