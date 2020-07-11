0668bd7fc06230b3facde0289edd3aeb2112f3b2
Yevstar <fedchyshynyevhen@gmail.com>
2020-07-11 18:28:10 +1000

    remove unneeded files

--- sql/3.0.7/0.1.9.sql
+++
@@ -1,4 +0,0 @@
-alter table news ADD recipientRefId int null;
-alter table news ADD `published_at` TIMESTAMP NULL AFTER `updated_at`;
-alter table news alter column isActive set default 0;
-alter table news alter column isNotification set default 0;
--- sql/3.0.7_gcp/0.2.2.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE wsa.banner MODIFY COLUMN `sequence` INT NULL;
-
-ALTER TABLE wsa.playerMinuteTracking DROP COLUMN playedFullPeriod;
-ALTER TABLE wsa.playerMinuteTracking ADD playedInPeriod BOOL NULL;
-ALTER TABLE wsa.playerMinuteTracking ADD playedEndPeriod BOOL NULL;
