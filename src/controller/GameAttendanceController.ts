ea0b133f5ecb688de22af930b23ef5c83e8916a8
Yevstar <fedchyshynyevhen@gmail.com>
2020-08-26 06:09:27 +0000

    Merged in feature/LWA-618-manual-attendance-upload (pull request #283)
    commit 0084d9c1 
    Author: uppalapati dileep varma <dileepvarmau@gmail.com-at-641701330719> 
    Date: Wed Aug 26 2020 11:05:39 GMT+0530 (India Standard Time) 
    
        Lineup playing value setting.
    
    
    commit b1b158bd 
    Author: uppalapati dileep varma <dileepvarmau@gmail.com-at-641701330719> 
    Date: Wed Aug 26 2020 10:50:56 GMT+0530 (India Standard Time) 
    
        Added verifiedBy as well for lineup from the gta.
    
    
    commit 032d1ce8 
    Author: uppalapati dileep varma <dileepvarmau@gmail.com-at-641701330719> 
    Date: Wed Aug 26 2020 10:43:56 GMT+0530 (India Standard Time) 
    
        Saving gta when bool condition true.
    
    
    commit 2362ea31 
    Author: uppalapati dileep varma <dileepvarmau@gmail.com-at-641701330719> 
    Date: Wed Aug 26 2020 09:06:33 GMT+0530 (India Standard Time) 
    
        If competition lineup is enabled first creating the lineups and then gta.
    
    
    commit 95e9a543 
    Author: uppalapati dileep varma <dileepvarmau@gmail.com-at-641701330719> 
    Date: Wed Aug 26 2020 07:16:26 GMT+0530 (India Standard Time) 
    
        Saving Game time attendance in the manual upload.

--- src/controller/GameAttendanceController.ts
+++ src/controller/GameAttendanceController.ts
@@ -15,9 +15,12 @@ import {GameTimeAttendance} from "../models/GameTimeAttendance";
 import {Response} from "express";
 import {BaseController} from "./BaseController";
 import {User} from "../models/User";
+import {Match} from "../models/Match";
+import {Lineup} from "../models/Lineup";
 import {TeamPlayerActivity} from "../models/views/TeamPlayerActivity";
 import { isNotNullAndUndefined } from "../utils/Utils"
 import{ GamePosition } from "../models/GamePosition";
+import {logger} from "../logger";
 
 @Authorized()
 @JsonController('/gtattendances')
@@ -105,68 +108,98 @@ export class GameAttendanceController extends BaseController {
         console.time('matchService.findById');
         let match = await this.matchService.findById(matchId);
         console.timeEnd('matchService.findById');
-        let gsPlayerId;
-        let gaPlayerId;
         if (match.matchStatus != 'ENDED') {
             if (attendance.length > 0) {
-                let save: GameTimeAttendance[] = [];
-                for (let att of attendance) {
-                    let gta = await this.gameTimeAttendanceService.findPlayerByParamAndLast(matchId, teamId, att.playerId);
-                    if (gta) {
-                        if ((att.period && gta.period != att.period) ||
-                            (att.positionId && gta.positionId != att.positionId) ||
-                            (att.isPlaying && gta.isPlaying != att.isPlaying)) {
-                            save.push(this.gameTimeAttendanceService.prepare(matchId, teamId, period, att, user.id));
-                        }
-                    } else {
-                        save.push(this.gameTimeAttendanceService.prepare(matchId, teamId, period, att, user.id));
-                    }
-                    if (updateMatchEvents) {
-                        /// Checking for any player with position Goal shooter or
-                        /// goal attack
-                        if (att.positionId == GamePosition.GOAL_SHOOTER) {
-                            gsPlayerId = att.playerId;
-                        } else if (att.positionId == GamePosition.GOAL_ATTACK) {
-                            gaPlayerId = att.playerId;
-                        }
-                    }
+                try {
+                   await this.updateGameTimeAttendances(
+                      match,
+                      teamId,
+                      attendance,
+                      period,
+                      updateMatchEvents,
+                      user
+                   );
+                } catch (err) {
+                    logger.error(`Failed upload of attendances due to -`, err);
+                    return response.status(400).send({
+                        err, name: 'unexpected_error', message: 'Failed to save the attendances.'
+                  });
                 }
-                await this.gameTimeAttendanceService.batchUpdate(save);
-                if (updateMatchEvents) {
-                    let team = match.team1Id == teamId ? 'team1' : 'team2';
-                    if (isNotNullAndUndefined(gsPlayerId)) {
-                        this.matchService.updateMatchStatEvent(
-                            matchId,
-                            team,
-                            GamePosition.GOAL_SHOOTER,
-                            gsPlayerId
-                        );
-                    } else if(isNotNullAndUndefined(gaPlayerId)) {
-                        this.matchService.updateMatchStatEvent(
-                            matchId,
-                            team,
-                            GamePosition.GOAL_ATTACK,
-                            gaPlayerId
-                        );
-                    }
-                }
-                let tokens = (await this.deviceService.findScorerDeviceFromRoster(matchId)).map(device => device.deviceId);
-                if (tokens && tokens.length > 0) {
-                    this.firebaseService.sendMessage({
-                        tokens: tokens,
-                        data: {
-                            type: 'attendance_added',
-                            matchId: matchId.toString(),
-                            teamId: teamId.toString()
-                        }
-                    })
-                }
+
                 return response.status(200).send({message: 'Players updated'});
             } else {
                 return response.status(400).send({name: 'updated_attendance_error', message: 'List attendance is empty'});
             }
         } else {
             return response.status(400).send({name: 'update_attendance_error', message: 'Attendance cannot be submitted after a match has ended'});
+        }
+    }
+
+    private async updateGameTimeAttendances(
+        match: Match,
+        teamId: number,
+        attendances: GameTimeAttendance[],
+        period: number,
+        updateMatchEvents: boolean,
+        user: User,
+    ) {
+        let gsPlayerId;
+        let gaPlayerId;
+
+        let save: GameTimeAttendance[] = [];
+        for (let att of attendances) {
+            if (!period) {
+                period = att.period;
+            }
+            let gta = await this.gameTimeAttendanceService.findPlayerByParamAndLast(match.id, teamId, att.playerId);
+            if (gta) {
+                if ((att.period && gta.period != att.period) ||
+                    (att.positionId && gta.positionId != att.positionId) ||
+                    (att.isPlaying && gta.isPlaying != att.isPlaying)) {
+                    save.push(this.gameTimeAttendanceService.prepare(match.id, teamId, period, att, user.id));
+                }
+            } else {
+                save.push(this.gameTimeAttendanceService.prepare(match.id, teamId, period, att, user.id));
+            }
+            if (updateMatchEvents) {
+                /// Checking for any player with position Goal shooter or
+                /// goal attack
+                if (att.positionId == GamePosition.GOAL_SHOOTER) {
+                    gsPlayerId = att.playerId;
+                } else if (att.positionId == GamePosition.GOAL_ATTACK) {
+                    gaPlayerId = att.playerId;
+                }
+            }
+        }
+        await this.gameTimeAttendanceService.batchUpdate(save);
+        if (updateMatchEvents) {
+            let team = match.team1Id == teamId ? 'team1' : 'team2';
+            if (isNotNullAndUndefined(gsPlayerId)) {
+                this.matchService.updateMatchStatEvent(
+                    match.id,
+                    team,
+                    GamePosition.GOAL_SHOOTER,
+                    gsPlayerId
+                );
+            } else if(isNotNullAndUndefined(gaPlayerId)) {
+                this.matchService.updateMatchStatEvent(
+                    match.id,
+                    team,
+                    GamePosition.GOAL_ATTACK,
+                    gaPlayerId
+                );
+            }
+        }
+        let tokens = (await this.deviceService.findScorerDeviceFromRoster(match.id)).map(device => device.deviceId);
+        if (tokens && tokens.length > 0) {
+            this.firebaseService.sendMessage({
+                tokens: tokens,
+                data: {
+                    type: 'attendance_added',
+                    matchId: match.id.toString(),
+                    teamId: teamId.toString()
+                }
+            })
         }
     }
 
@@ -188,5 +221,81 @@ export class GameAttendanceController extends BaseController {
     @Delete('/:id')
     async deleteById(@Param("id") id: number) {
         return this.gameTimeAttendanceService.deleteById(id);
+    }
+
+    @Post('/manualUpload')
+    async manualUpload(
+        @HeaderParam("authorization") user: User,
+        @QueryParam('matchId') matchId: number,
+        @QueryParam('teamId') teamId: number,
+        @QueryParam('saveAttendance') saveAttendance: boolean = true,
+        @Body() attendance: GameTimeAttendance[],
+        @Res() response: Response
+    ): Promise<any> {
+        if (!matchId) return response.status(400)
+            .send({name: 'validation_error', message: 'Match id required'});
+        if (!teamId) return response.status(400)
+            .send({name: 'validation_error', message: 'Team id required'});
+
+        const match = await this.matchService.findById(matchId);
+        const comp = await this.competitionService.findById(match.competitionId);
+        if (attendance.length > 0) {
+            /// Line up
+            if (comp.lineupSelectionEnabled == true) {
+                var lineupArray = new Array();
+                for (let att of attendance) {
+                    let lineup: Lineup = new Lineup();
+                    lineup.matchId = att.matchId;
+                    lineup.competitionId = comp.id;
+                    lineup.teamId = att.teamId;
+                    lineup.playerId = att.playerId;
+                    lineup.positionId = 0;
+                    lineup.playing = att.isPlaying;
+                    lineup.borrowed = att.isBorrowed;
+                    lineup.verifiedBy = att.verifiedBy;
+                    lineupArray.push(lineup);
+                }
+
+                try {
+                    if (lineupArray) {
+                        await this.matchService.deleteLineups(matchId, teamId);
+                        await this.matchService.batchSaveLineups(lineupArray);
+                    } else {
+                        return response.status(400).send({
+                            name: 'validation_error',
+                            message: `Unable to create lineups`
+                        });
+                    }
+                } catch (e) {
+                    logger.error(`Failed to create lineup due to -`, e);
+                    return response.status(400).send({
+                        name: 'updated_attendance_error',
+                        message: `Failed to create lineup`
+                    });
+                }
+            }
+            if (saveAttendance) {
+              /// Game time attendance
+              try {
+                 await this.updateGameTimeAttendances(
+                    match,
+                    teamId,
+                    attendance,
+                    null,
+                    true,
+                    user
+                 );
+              } catch (err) {
+                  logger.error(`Failed upload of attendances due to -`, err);
+                  return response.status(400).send({
+                      err, name: 'unexpected_error', message: 'Failed to save the attendances.'
+                });
+              }
+            }
+
+            return response.status(200).send({message: 'Success'});
+        } else {
+            return response.status(400).send({name: 'updated_attendance_error', message: 'List attendance is empty'});
+        }
     }
 }
