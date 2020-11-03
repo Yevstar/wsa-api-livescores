9124fe94fee232e936e7ac94965d31f4505f0c72
Yevstar <fedchyshynyevhen@gmail.com>
2020-11-03 23:33:52 +1100

    1/6th match sheet

--- src/utils/MatchSheetTemplate.ts
+++ src/utils/MatchSheetTemplate.ts
@@ -303,10 +303,12 @@ const getMatchSheetTemplate = (
                 border-bottom: 1px solid black;
              }
            }`
-           ) : (
+           ) : ''} 
+
+          ${templateType == 'Simple'? (
             `body {
-                font-family: Arial
-             }
+               font-family: Arial
+            }
             .page {
                width: 100%;
                max-width: 800px;
@@ -369,8 +371,8 @@ const getMatchSheetTemplate = (
                box-sizing: border-box;
             }
             .infodiv {
-                font-size: 12px;
-                margin-bottom: 4px;
+                  font-size: 12px;
+                  margin-bottom: 4px;
             }
             .tableContent {
                width: 100%;
@@ -388,9 +390,9 @@ const getMatchSheetTemplate = (
                box-sizing: border-box;
             }
             #attendance2 {
-                width: 49%;
-                padding: 0 16px 0 4px;
-                box-sizing: border-box;
+                  width: 49%;
+                  padding: 0 16px 0 4px;
+                  box-sizing: border-box;
             }
             .table {
                border: 1px solid black;
@@ -425,47 +427,19 @@ const getMatchSheetTemplate = (
                text-align: left;
             }
             #signature {
-                width: 20%;
+                  width: 20%;
             }
             #player {
-                width: 60%;
+                  width: 60%;
             }
             #playerid, #quarter3, #quarter4 {
-                display: none;
-                width: 0%;
+                  display: none;
+                  width: 0%;
             }
             .subTitle {
                padding: 10px 9px 4px 16px;
                font-size: 12px;
                box-sizing: border-box;
-            }
-            .passCell:not(:last-child) {
-               border-right: 1px solid black;
-            }
-            .passCell {
-               width: 2.308%;
-               text-align: center;
-            }
-            .passTable {
-               width: 100%;
-               padding-left: 16px;
-               padding-right: 24px;
-               box-sizing: border-box;
-            }
-            .passTable .table {
-               border-right: 1px solid black;
-            }
-            .passRow {
-               width: 100%;
-               height: 12px;
-               display: -webkit-box;
-               display: -webkit-flex;
-               -webkit-flex-wrap: wrap;
-               display: flex;
-               flex-wrap: wrap;
-               flex-direction: row;
-               font-size: 9px;
-               border-bottom: 1px solid black;
             }
             .tableTitle {
                font-size: 9px;
@@ -519,48 +493,10 @@ const getMatchSheetTemplate = (
                border-right: 1px solid black;
                box-sizing: border-box;
             }
-            .goalTable {
-               width: 100%;
-               padding: 8px 24px 8px 16px;
-               box-sizing: border-box;
-            }
-            .goalTable .table {
-               border-right: 1px solid black;
-            }
-            .goalRow {
-               width: 100%;
-               height: 24px;
-               display: -webkit-box;
-               display: -webkit-flex;
-               -webkit-flex-wrap: wrap;
-               display: flex;
-               flex-wrap: wrap;
-               flex-direction: row;
-               font-size: 8px;
-               border-bottom: 1px solid black;
-            }
-            .goalCell {
-               width: 5.85%;
-               border-right: 1px solid black;
-               text-align: center;
-            }
-            .goalCheckCell {
-               width: 29%;
-               border-right: 1px solid black;
-            }
-            .goalSubCell {
-               height: 12px;
-               text-align: center;
-               border-bottom: 1px solid black;
-            }
             #scorers {
                display:none;
             }
-          }`
-          )}
-
-          ${templateType == 'Simple'? (
-            `.scoreTableRight {
+            .scoreTableRight {
                width: 50%;
                padding: 4px 24px 4px 8px;
                box-sizing: border-box;
@@ -585,11 +521,199 @@ const getMatchSheetTemplate = (
                flex-wrap: wrap;
                flex-direction: row;
                font-size: 16px;
-            }`
-          ) : ''}
+            }`) : ''}
 
           ${templateType == 'Scorecard'? (
-            `.scoreTableRight {
+            `
+            body {
+               font-family: Arial
+            }
+            .page {
+               width: 100%;
+               max-width: 800px;
+               max-height:350px;
+               padding: 40px 2px 2px 2px;
+               background-color: #FFFFFF;
+               box-sizing: border-box;
+               page-break-before:avoid;
+            }
+            .document {
+               width: 100%;
+            }
+            .header {
+               padding: 0 16px;
+               margin-bottom: 8px;
+               width: 100%;
+               display: -webkit-box;
+               display: -webkit-flex;
+               -webkit-flex-wrap: wrap;
+               display: flex;
+               flex-wrap: wrap;
+               flex-direction: row;
+               justify-content: space-between;
+               box-sizing: border-box;
+            }
+            .associationName {
+               font-size: 14px;
+               font-weight: bold;
+               padding: 8px 0;
+               box-sizing: border-box;
+            }
+            .templateType {
+               font-size: 12px;
+            }
+            .title {
+               width: 50%;
+            }
+            .logo {
+               width: 50px;
+               height: 50px;
+               margin-left: auto;
+               margin-right: 16px;
+            }
+            .matchInfo {
+               padding: 8px 16px;
+               display: -webkit-box;
+               display: -webkit-flex;
+               -webkit-flex-wrap: wrap;
+               display: flex;
+               flex-wrap: wrap;
+               flex-direction: row;
+               box-sizing: border-box;
+            }
+            .infoContentLeft {
+               width: 50%;
+               box-sizing: border-box;
+            }
+            .infoContentRight {
+               width: 50%;
+               padding-left: 8px;
+               box-sizing: border-box;
+            }
+            .infodiv {
+                  font-size: 12px;
+                  margin-bottom: 4px;
+            }
+            .tableContent {
+               width: 100%;
+               display: -webkit-box;
+               display: -webkit-flex;
+               -webkit-flex-wrap: wrap;
+               display: flex;
+               flex-wrap: wrap;
+               flex-direction: row;
+               box-sizing: border-box;
+            }
+            #attendance1 {
+               width: 49%;
+               padding: 0 0 0 16px;
+               box-sizing: border-box;
+            }
+            #attendance2 {
+                  width: 49%;
+                  padding: 0 16px 0 4px;
+                  box-sizing: border-box;
+            }
+            .table {
+               border: 1px solid black;
+               border-right: 0;
+               border-bottom: 0;
+            }
+            .row {
+               width: 100%;
+               height: 20px;
+               display: -webkit-box;
+               display: -webkit-flex;
+               -webkit-flex-wrap: wrap;
+               display: flex;
+               flex-wrap: wrap;
+               flex-direction: row;
+               font-size: 12px;
+               border-bottom: 1px solid black;
+               border-right: 1px solid black;
+            }
+            #quarter1 {
+               border-right: 1px solid black;
+            }
+            .cell {
+               width: 8.54%;
+               padding-top: 2px;
+               text-align: center;
+            }
+            .largeCell {
+               padding-top: 2px;
+               padding-left: 2px;
+               border-right: 1px solid black;
+               text-align: left;
+            }
+            #signature {
+                  width: 20%;
+            }
+            #player {
+                  width: 60%;
+            }
+            #playerid, #quarter3, #quarter4 {
+                  display: none;
+                  width: 0%;
+            }
+            .subTitle {
+               padding: 10px 9px 4px 16px;
+               font-size: 12px;
+               box-sizing: border-box;
+            }
+            .tableTitle {
+               font-size: 9px;
+               margin-bottom: 12px;
+            }
+            .summaryTable {
+               width: 100%;
+               padding: 16px 24px 4px 16px;
+               box-sizing: border-box;
+            }
+            .summaryRow {
+               width: 100%;
+               height: 36px;
+               display: -webkit-box;
+               display: -webkit-flex;
+               -webkit-flex-wrap: wrap;
+               display: flex;
+               flex-wrap: wrap;
+               flex-direction: row;
+               font-size: 12px;
+               border-bottom: 1px solid black;
+            }
+            .summaryCell {
+               width: 28.5%;
+               padding-left: 12px;
+               padding-top: 2px;
+               box-sizing: border-box;
+            }
+            .gapCell {
+               width: 0.8%;
+               border-right: 1px solid black;
+               text-align: center;
+            }
+            .signatureCell {
+               width: 21%;
+               padding-top: 2px;
+               border-right: 1px solid black;
+               box-sizing: border-box;
+            }
+            .voteCell {
+               width: 20%;
+               padding-left: 12px;
+               padding-top: 2px;
+               border-right: 1px solid black;
+               box-sizing: border-box;
+            }
+            .teamCell {
+               width: 40%;
+               padding-left: 12px;
+               padding-top: 2px;
+               border-right: 1px solid black;
+               box-sizing: border-box;
+            }
+            .scoreTableRight {
                width: 50%;
                padding: 4px 24px 4px 8px;
                box-sizing: border-box;
@@ -604,7 +728,6 @@ const getMatchSheetTemplate = (
                width: 10%;
                text-align: center;
                border: 1px solid black;
-               color: #fff;
             }
             .scoreRow {
                width: 100%;
@@ -646,13 +769,13 @@ const getMatchSheetTemplate = (
             ) : (
             `<div class="matchInfo no-break">
                <div class="infoContentLeft">
-                  <div class="infodiv">${match.round ? match.round.name : ''} - Court ${match.venueCourt?match.venueCourt.courtNumber:""} - ${match.division?match.division.divisionName:""}</div>
-                  <div class="infodiv">${match.venueCourt && match.venueCourt.venue ? match.venueCourt.venue.name : ''}</div>
+                  <div class="infodiv">${match.competition ? match.competition.name : ''}</div>
+                  <div class="infodiv">Time: ${matchStartTime}</div>
                   <div class="infodiv">${match.team1 ? match.team1.name : ''}</div>
                </div>
                <div class="infoContentRight">
-                  <div class="infodiv">Date: ${matchDate}</div>
-                  <div class="infodiv">Time: ${matchStartTime}</div>
+                  <div class="infodiv">${match.round ? match.round.name : ''}</div>
+                  <div class="infodiv">Field: ${match.venueCourt?match.venueCourt.name:""}</div>
                   <div class="infodiv">${match.team2 ? match.team2.name : ''}</div>
                </div>
             </div>`
@@ -725,7 +848,7 @@ const getMatchSheetTemplate = (
                 </div>
             </div>`
             ) : ''}
-            ${templateType !== 'Simple' ? (
+            ${templateType !== 'Simple' && templateType !== 'Scorecard'? (
             `<div class="subTitle">Progressive Score</div>
             <div class="tableContent">
                 <div class="scoreTableLeft">
@@ -749,7 +872,8 @@ const getMatchSheetTemplate = (
                     )).join('')}
                 </div>
             </div>`
-            ) : (
+            ) : '' }
+            ${templateType == 'Simple' ? (
                 `<div class="subTitle">Progressive Score</div>
                 <div class="tableContent">
                     <div class="scoreTableLeft">
@@ -773,7 +897,31 @@ const getMatchSheetTemplate = (
                         )).join('')}
                     </div>
                 </div>`
-            ) }
+            ) : '' }
+            ${templateType == 'Scorecard' ? (
+               `<div class="tableContent">
+                   <div class="scoreTableLeft">
+                       <div class="tableTitle">Team 1</div>
+                       ${[...Array(4).keys()].map((rowIndex) => (
+                           `<div class="scoreRow">
+                             ${[...Array(7).keys()].map((cellIndex) => (
+                                 `<div class="scoreCell">&nbsp;</div>`
+                               )).join('')}
+                           </div>`
+                       )).join('')}
+                   </div>
+                   <div class="scoreTableRight">
+                       <div class="tableTitle">Team 2</div>
+                       ${[...Array(4).keys()].map((rowIndex) => (
+                           `<div class="scoreRow">
+                               ${[...Array(7).keys()].map((cellIndex) => (
+                                   `<div class="scoreCell">&nbsp;</div>`
+                               )).join('')}
+                           </div>`
+                       )).join('')}
+                   </div>
+               </div>`
+           ) : '' }
             ${templateType !== 'Social' && templateType !== 'Simple' && templateType !== 'Scorecard' ? (
                 `<div>
                     <div class="subTitle">Goal Statistics</div>
