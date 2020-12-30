ce5416e7d9b4675803bde5b08f20360aeea270e9
Yevstar <fedchyshynyevhen@gmail.com>
2020-12-30 17:03:52 +0200

    Update umpire allocation settings model

--- src/models/UmpireAllocationSetting.ts
+++ src/models/UmpireAllocationSetting.ts
@@ -1,14 +1,11 @@
-import {
-    Column,
-    Entity, JoinColumn, ManyToOne,
-} from "typeorm-plus";
+import {BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, ManyToOne,} from "typeorm-plus";
 import {IsBoolean, IsNumber} from "class-validator";
 import {Competition} from "./Competition";
 import {BaseUmpireAllocationSetting} from "./BaseUmpireAllocationSetting";
 import {UmpireAllocatorTypeEnum} from "./enums/UmpireAllocatorTypeEnum";
 import {UmpireAllocationTypeEnum} from "./enums/UmpireAllocationTypeEnum";
 
-@Entity({ name: "UmpireAllocationSettings" })
+@Entity()
 export class UmpireAllocationSetting extends BaseUmpireAllocationSetting {
 
     @Column()
@@ -27,9 +24,24 @@ export class UmpireAllocationSetting extends BaseUmpireAllocationSetting {
     @IsBoolean()
     activateCoaches: boolean;
 
+    @Column({nullable: true})
+    timeBetweenMatches: number;
+
+    @Column({nullable: true})
+    maxNumberOfMatches: number;
+
     @ManyToOne(type => Competition, competition => competition.umpireAllocationSettings)
     @JoinColumn({
         name: "competitionId"
     })
     competition: Competition;
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    checkAllocationType() {
+        if (this.umpireAllocationTypeRefId != UmpireAllocationTypeEnum.VIA_POOLS && this.umpireAllocationTypeRefId != UmpireAllocationTypeEnum.OWN_ORGANISATION) {
+            this.timeBetweenMatches = null;
+            this.maxNumberOfMatches = null;
+        }
+    }
 }
--- src/models/enums/UmpireAllocationTypeEnum.ts
+++ src/models/enums/UmpireAllocationTypeEnum.ts
@@ -1,6 +1,6 @@
 export enum UmpireAllocationTypeEnum {
-    'MANUALLY_ALLOCATE' = 242,
+    'MANUALLY_ALLOCATED' = 242,
     'VIA_POOLS' = 243,
-    'RANDOM_ALLOCATION' = 244,
-    'LINK_TO_SAME_TEAM' = 245,
+    'OWN_TEAM' = 244,
+    'OWN_ORGANISATION' = 245,
 }
