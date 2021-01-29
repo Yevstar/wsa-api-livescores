2b5a330e6a5731526bfa900dd4c488d90de635f2
Yevstar <fedchyshynyevhen@gmail.com>
2021-01-29 17:00:57 +0200

    show only affiliated umpires for participating-in organisations

--- src/services/UmpireService.ts
+++ src/services/UmpireService.ts
@@ -6,36 +6,66 @@ import {Competition} from "../models/Competition";
 import * as utils from '../utils/Utils';
 import {CrudResponse} from "../controller/dto/CrudResponse";
 import {Umpire} from "../models/Umpire";
+import {Inject} from "typedi";
+import CompetitionService from "./CompetitionService";
+import {EntityType} from "../models/security/EntityType";
+import {Location} from "../models/Location";
+import {CompetitionOrganisation} from "../models/CompetitionOrganisation";
+import {Role} from "../models/security/Role";
+import {UserRoleEntity} from "../models/security/UserRoleEntity";
 
 export class UmpireService extends BaseService<User> {
     modelName(): string {
         return "Umpire";
     }
 
-    async findManyByCompetitionId(
+    @Inject()
+    private readonly competitionService: CompetitionService;
+
+    async findManyByCompetitionIdForOrganisation(
         competitionId: number,
+        organisationId: number,
         offset: number = 0,
         limit: number = 10,
     ): Promise<CrudResponse<any>> {
+        const competition = await this.entityManager.findOneOrFail(Competition, competitionId);
+        const isCompetitionOrganizer = await this.competitionService.isCompetitionOrganiser(organisationId, competitionId);
+        let competitionOrganizationsQuery = this.entityManager.createQueryBuilder(CompetitionOrganisation, 'compOrg');
+
+        if (isCompetitionOrganizer) {
+            competitionOrganizationsQuery
+                .where("compOrg.competitionId = :competitionId", {
+                    competitionId,
+                });
+        } else {
+            competitionOrganizationsQuery
+                .where("compOrg.orgId = :orgId AND compOrg.competitionId = :competitionId", {
+                    orgId: organisationId,
+                    competitionId,
+                });
+        }
+
+        const compOrgs = await competitionOrganizationsQuery.getMany();
+
+
         const query = this.entityManager.createQueryBuilder(User,"u")
             .leftJoinAndSelect("u.userRoleEntities", "roles")
             .leftJoinAndSelect("u.umpireCompetitionRank", "umpireCompetitionRank")
             .leftJoinAndSelect("umpireCompetitionRank.competition", "umpireCompetitionRank.competition")
             .loadRelationCountAndMap('u.matchesCount', 'u.matchUmpires')
-            .where("roles.entityTypeId = :entityTypeId AND roles.entityId = :entityId AND roles.roleId IN (15,20)", {
-                entityTypeId: 1,
-                entityId: competitionId,
+            .where("roles.entityTypeId = :entityTypeId AND roles.entityId IN (:compOrgIds) AND roles.roleId IN (:roles)", {
+                entityTypeId: EntityType.COMPETITION_ORGANISATION,
+                compOrgIds: compOrgs.map(compOrg => compOrg.id),
+                roles: [Role.UMPIRE, Role.UMPIRE_COACH],
             });
 
         const total = await query.getCount();
-
         query.take(limit).skip(offset)
 
         const umpires = await query.getMany();
         for (const umpire of umpires) {
             umpire.rank = this.calculateAverageRank(umpire);
         }
-
         return {
             ...utils.paginationData(total, limit, offset),
             data: umpires,
